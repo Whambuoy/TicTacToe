ca674cd6ae1e5399cdefa9d95d984fd828973772
Whambuoy <esther25maina@gmail.com>
2018-04-08 14:39:52 +0300

    2-player xml layout for 3 by 3

---
+++ app/src/main/java/com/example/android/tictactoe/TwoPlayer3By3.java
@@ -0,0 +1,13 @@
+package com.example.android.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoPlayer3By3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player3_by3);
+    }
+}
